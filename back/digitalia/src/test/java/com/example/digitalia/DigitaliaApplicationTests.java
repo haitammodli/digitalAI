b6f759b8f0cbc81b703787fdae1559be099fb4ae
HaitamP <haytam.modli2004@gmail.com>
2025-07-25 18:16:40 +0100

    first commit

---
+++ back/digitalia/src/test/java/com/example/digitalia/DigitaliaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.digitalia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitaliaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
